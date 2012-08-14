2a8b625d5214900b4cc9ff20d453da0d43f2e74b
Jonathan Roy <jroy@optimumweb.ca>
2012-08-14 16:20:25 -0400

    Style

---
+++ banner.php
@@ -0,0 +1,5 @@
+<section id="banner">
+	<div class="container <?php wpbp_option('container_class'); ?>">
+		<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+	</div>
+</section>
