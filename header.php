253a8f3d1b34bc50ed13b5ba1e09266e9b4398ee
Jonathan Roy <jroy@optimumweb.ca>
2012-08-14 15:04:39 -0400

    Add banner

--- header.php
+++ header.php
@@ -7,8 +7,20 @@
             <div class="container <?php wpbp_option('container_class'); ?>">
             	<div class="grid_6">
 					<section id="site-title">
-						<h1><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
-						<h3><?php bloginfo('description'); ?></h3>
+						<?php if ( of_get_option('logo') ) : ?>
+							<h1>
+								<a href="<?php echo home_url(); ?>/">
+									<img id="logo" src="<?php echo of_get_option('logo'); ?>" alt="<?php bloginfo('name'); ?>" />
+								</a>
+							</h1>
+						<?php else : ?>
+							<h1>
+								<a href="<?php echo home_url(); ?>/">
+									<?php bloginfo('name'); ?>
+								</a>
+							</h1>
+							<h3><?php bloginfo('description'); ?></h3>
+						<?php endif; ?>
 					</section>
 			    </div>
 			    <div class="grid_6">
@@ -18,3 +30,5 @@
             <?php wpbp_header_inside_after(); ?>
 		</header>
 		<?php wpbp_header_after(); ?>
+
+		<?php if ( is_front_page() ) get_template_part('banner'); ?>
