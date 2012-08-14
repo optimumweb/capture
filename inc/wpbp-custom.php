f546fdef94d3e4edf89ea92a824d3e89e7bd1aec
Jonathan Roy <jroy@optimumweb.ca>
2012-08-14 16:58:54 -0400

    Style

--- inc/wpbp-custom.php
+++ inc/wpbp-custom.php
@@ -18,17 +18,25 @@ function capture_style()
 {
 ?>
 <style>
+
 	body {
 		background-image: url(<?php echo of_get_option('background_image', THEME_URI . '/img/background_image.jpg'); ?>);
 		background-repeat: <?php echo of_get_option('background_repeat', 'repeat'); ?>;
 		background-attachment: <?php echo of_get_option('background_attachment', 'scroll'); ?>;
 	}
+
 	#wrap {
 		border-top: 2px solid <?php echo of_get_option('color_light', '#000'); ?>;
 	}
+
+	#header #call .tel {
+		color: <?php echo of_get_option('color_light', '#000'); ?>;
+	}
+
 	#banner {
 		background-color: <?php echo of_get_option('color_dark', '#000'); ?>;
 	}
+
 </style>
 <?php
 }
